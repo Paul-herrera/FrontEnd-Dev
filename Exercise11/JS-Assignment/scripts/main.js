b72f863b8bf07a8fe9e3b10c5e3b49f8291a105c
paulherrera <paulherrera89@gmail.com>
2018-09-26 22:21:45 -0500

    Exercise 11 and 12

--- Exercise11/JS-Assignment/scripts/main.js
+++ Exercise11/JS-Assignment/scripts/main.js
@@ -51,6 +51,8 @@ document.getElementById("p3_number").innerHTML= randomNumber;
 	// p3 bullet 2
 if (isNaN(randomNumber)) {
 	document.getElementById("p3_valid").innerHTML= "False";
+}else if(randomNumber.length >-1){
+	document.getElementById("p3_valid").innerHTML= "False";
 }else{
 	document.getElementById("p3_valid").innerHTML= "True";
 }
@@ -60,9 +62,15 @@ if (isNaN(randomNumber)) {
     console.log(arrayInput);
 
 	// p4 bullet 1
-arrayItems.push(arrayInput);
+	if (arrayItems.length >-1){
+		var arrayField = document.getElementById("arrayValue");
+    arrayField.classList.add("invalid");
+	} else{
+		//I dont know why this doesnt work???
+	arrayItems.push(arrayInput);
 	console.log(arrayItems);
-
+	}
+	
 	// p4 bullet 2
 var p4_arraylength = arrayItems.length;
 	document.getElementById("p4_arraylength").innerHTML= p4_arraylength;
@@ -70,9 +78,38 @@ var p4_arraylength = arrayItems.length;
 	//p4 bullet 3
 document.getElementById("p4_valid").innerHTML= arrayItems;
 
+	// Additional Challenge
+if (firstName.length <=0){
+    var fNameField = document.getElementById("firstName");
+    fNameField.classList.add("invalid");
+}
 
+if (lastName.length <=0){
+    var lNameField = document.getElementById("lastName");
+    lNameField.classList.add("invalid");
+}
+	
+if (!p2_valid){
+    var emAddressField = document.getElementById("emailAddress");
+    emAddressField.classList.add("invalid");
+}
 
-
+if (isNaN(randomNumber)){
+    var numberField = document.getElementById("randomNumber");
+    numberField.classList.add("invalid");
+} else if (randomNumber.length >-1){
+	var number_Field = document.getElementById("randomNumber");
+    number_Field.classList.add("invalid");
+}
+	
+	// Additional Challenge Bullet 2
+	
+	//I dont know why this doesnt work???
+function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.remove("mystyle");
+}
+	
     ////////////////////////Your code ends here.
 
 }
---
+++ Exercise12/JS-Assignment/scripts/main.js
@@ -0,0 +1,72 @@
+function update() {
+    /* ===================
+        Your code starts here
+    =====================*/
+	
+    //Create new array.
+var message = [];
+    //If first name is empty. Add it to the array
+	
+if (!getValue('firstName')) {
+	message.push("First Name is required");
+}
+	
+    //If last name is empty. Add it to the array
+if (!getValue('lastName')) {
+	message.push("Last Name is required");
+}
+
+    //Email is empty. Add it to the array
+if (!getValue('emailAddress')) {
+	message.push("Email is required");
+}
+
+    //Loop through array
+var htmlString = "";
+for( var i = 0; i < message.length; i++) {
+	htmlString = htmlString + '<p>' + message[i] + '</p>';
+}
+	getElm("error-list").innerHTML = htmlString;
+
+    //Create javascript object for the user
+var user = {
+	firstName: getElm('firstName'),
+	lastName: getElm('lastName'),
+	email: getElm('emailAddress'),
+	fullname: function(){
+		var field = this;
+		return field.firstName + " " + field.lastName;
+	}
+};
+	
+    //log the user object to the console
+console.log(user);
+
+    //Check to see if everything is valid
+if (message.length <=0){
+	
+    //Create string for today's date. Remember, if you hard code this, it will be invalid tomorrow.
+	var submittedDate = new Date().toDateString();
+	
+    //Make the alert box.
+	window.alert("Submitted on " + submittedDate);
+}
+    /* ===================
+        Your code ends here
+    =====================*/
+
+
+}
+
+
+
+var btn = document.getElementById('btnUpdate');
+btn.addEventListener('click', update);
+
+function getValue(id) {
+    return getElm(id).value;
+}
+
+function getElm(id) {
+    return document.getElementById(id);
+}
